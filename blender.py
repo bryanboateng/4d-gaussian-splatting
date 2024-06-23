5cfac52cdf238085285d4838d060032710d95712
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2024-06-23 13:31:07 +0200

    dfsf

--- blender.py
+++ blender.py
@@ -2,10 +2,10 @@ import bpy
 import math
 import os
 
-output_path = "/Users/bryanboateng/Uni/moin/4d-gaussian-splatting"
+blend_file_path = "/Users/bryanboateng/Uni/4d-gaussian-splatting/santa.blend"
+output_directory_path = "/Users/bryanboateng/Uni/4d-gaussian-splatting/data/santa"
 
-bpy.ops.object.select_all(action="SELECT")
-bpy.ops.object.delete()
+bpy.ops.wm.open_mainfile(filepath=blend_file_path)
 
 cameras = []
 camera_count = 12
@@ -18,28 +18,18 @@ for i in range(camera_count):
     )
     bpy.context.collection.objects.link(camera)
     camera.location = (x, y, 0)
-    camera.rotation_euler = (math.pi / 2,  0, angle + math.pi / 2)
+    camera.rotation_euler = (math.pi / 2, 0, angle + math.pi / 2)
     cameras.append(camera)
 
-
-bpy.ops.mesh.primitive_monkey_add()
-obj = bpy.context.object
-
-light = bpy.data.objects.new(name="Sun_Light", object_data=(bpy.data.lights.new(name="Sun_Light", type='SUN')))
-bpy.context.collection.objects.link(light)
-light.location = (0, 0, 10)
-light.rotation_euler = (math.radians(45), math.radians(-45), 0)
-
-bpy.context.scene.frame_start = 1
-bpy.context.scene.frame_end = 2
+frame_start = bpy.context.scene.frame_start
+frame_end = bpy.context.scene.frame_end
 
 bpy.context.scene.render.image_settings.file_format = "JPEG"
-
-for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end + 1):
+for frame in range(frame_start, 2):
     bpy.context.scene.frame_set(frame)
     for index, camera in enumerate(cameras):
         bpy.context.scene.camera = camera
         bpy.context.scene.render.filepath = os.path.join(
-            output_path, "ims", str(index), f"{frame:06}.jpg"
+            output_directory_path, "ims", str(index), f"{frame - 1:06}.jpg"
         )
         bpy.ops.render.render(write_still=True)
